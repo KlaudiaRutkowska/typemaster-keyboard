a289adae13af7a636393c8812758a1a5a071cbce
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2024-01-20 22:53:02 +0100

    feat(component): add header component

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,47 @@
 import '../styles/style.scss'
+
+const header = document.querySelector('header')
+let headerHeight = getHeaderHeight(header)
+let paddingBlock = getPaddingBlock(header)
+let halfPaddingBlock = paddingBlock / 3
+
+window.addEventListener('resize', () => {
+    console.log('resize')
+
+    headerheight = getHeaderHeight(header)
+    paddingBlock = getPaddingBlock(header)
+    halfPaddingBlock = paddingBlock / 3
+
+    changePadding()
+})
+
+window.addEventListener('scroll', () => {
+    changePadding()
+})
+
+function changePadding() {
+    // const html = document.querySelector('html').scrollTop;
+    const scrolled = document.documentElement.scrollTop
+    console.log('padding-block: ', halfPaddingBlock)
+    console.log('half padding-block: ', paddingBlock)
+
+    if (scrolled > headerHeight) {
+        header.style.paddingBlock = halfPaddingBlock + 'px'
+    } else {
+        header.style.paddingBlock = paddingBlock + 'px'
+    }
+}
+
+function getHeaderHeight(header) {
+    return parseInt(
+        getComputedStyle(header).getPropertyValue('--header-height'),
+        10
+    )
+}
+
+function getPaddingBlock(header) {
+    return parseInt(
+        getComputedStyle(header).getPropertyValue('--padding-block'),
+        10
+    )
+}
