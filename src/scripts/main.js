a52407f898cc8ce01fd8a009bc065ad459f64cdd
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2024-01-20 23:15:36 +0100

    refactor: use scrolled class instead of javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,47 +1,10 @@
 import '../styles/style.scss'
 
 const header = document.querySelector('header')
-let headerHeight = getHeaderHeight(header)
-let paddingBlock = getPaddingBlock(header)
-let halfPaddingBlock = paddingBlock / 3
-
-window.addEventListener('resize', () => {
-    console.log('resize')
-
-    headerheight = getHeaderHeight(header)
-    paddingBlock = getPaddingBlock(header)
-    halfPaddingBlock = paddingBlock / 3
-
-    changePadding()
-})
 
 window.addEventListener('scroll', () => {
-    changePadding()
-})
-
-function changePadding() {
-    // const html = document.querySelector('html').scrollTop;
     const scrolled = document.documentElement.scrollTop
-    console.log('padding-block: ', halfPaddingBlock)
-    console.log('half padding-block: ', paddingBlock)
+    const headerHeight = parseInt(getComputedStyle(header).getPropertyValue('--header-height'), 10)
 
-    if (scrolled > headerHeight) {
-        header.style.paddingBlock = halfPaddingBlock + 'px'
-    } else {
-        header.style.paddingBlock = paddingBlock + 'px'
-    }
-}
-
-function getHeaderHeight(header) {
-    return parseInt(
-        getComputedStyle(header).getPropertyValue('--header-height'),
-        10
-    )
-}
-
-function getPaddingBlock(header) {
-    return parseInt(
-        getComputedStyle(header).getPropertyValue('--padding-block'),
-        10
-    )
-}
+    scrolled > headerHeight ? header.classList.add('scrolled') : header.classList.remove('scrolled')
+})
